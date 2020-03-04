d0b0a414ae6a53c15146a00190a0882a02588405
yellowmegaman <yellowmegaman@gmail.com>
2020-03-04 15:30:06 +0300

    add timestamps check instead of log check

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ CURL_HEADERS='-H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/v
 GITHUB_URL="https://api.github.com/repos"
 
 COMMIT_SHA=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_sha')
+COMMIT_TIMESTAMP=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_commit.timestamp')
 echo "COMMIT_SHA: $COMMIT_SHA"
 
 # Get all workflows that should be stopped
@@ -20,12 +21,11 @@ for wf in $workflows; do
 	runs=$(echo $runs $(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows/$wf/runs" | jq -r '.workflow_runs | .[] | select((.head_sha|test("'$COMMIT_SHA'")|not) and (.status|test("in_progress|queued"))) | .id'))
 done
 
-log=$(git log -n"$INPUT_GIT_LOG_COUNT" --format=format:%H | grep "$COMMIT_SHA" -A"$INPUT_GIT_LOG_COUNT")
 
 for run in $runs; do
 	echo "Possible target $run"
-	commit=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run" | jq -r '.head_sha')
-        if echo log | grep "$commit"; then
-		echo "Target: $run $commit"
+	run_timestamp=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run" | jq -r '.head_commit.timestamp')
+	if [[ "$COMMIT_TIMESTAMP" > "$run_timestamp" ]]; then
+		echo "Target: $run $run_timestamp"
 	fi
 done
