ea0dd5bd412e669944cc272fadbe70b4e03f6e4f
yellowmegaman <yellowmegaman@gmail.com>
2020-03-04 14:47:04 +0300

    fix wrong order

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,11 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   exit 1
 fi
 
-COMMIT_SHA=$(curl -s $CURL_HEADERS -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq '.head_sha')
-
-echo "COMMIT_SHA: $COMMIT_SHA"
-
 CURL_HEADERS='-H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json"'
 GITHUB_URL="https://api.github.com/repos"
+
+COMMIT_SHA=$(curl -s $CURL_HEADERS -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq '.head_sha')
+echo "COMMIT_SHA: $COMMIT_SHA"
 
 # Get all workflows that should be stopped
 workflows=$(curl -s $CURL_HEADERS -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows"  | jq '.workflows | .[] | select(.name|test("'$INPUT_WORKFLOWS_FILTER'")) | .id')
