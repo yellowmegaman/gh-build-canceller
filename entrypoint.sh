67ab0fd37cf32e86fc96921860ca88b3ef7865a5
yellowmegaman <yellowmegaman@gmail.com>
2020-03-04 11:09:35 +0300

    gh is a bit strange

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,13 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 fi
 
 GITHUB_URL="https://api.github.com/repos"
-
+COMMIT_SHA=$(git rev-parse --verify HEAD)
 
 # Get all workflows that should be stopped
 workflows=$(curl -s -H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows"  | jq '.workflows | .[] | select(.name|test("'$INPUT_WORKFLOWS_FILTER'")) | .id')
 
 for wf in $workflows; do
-	runs=$(echo $runs $(curl -s -H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows/$wf/runs" | jq '.workflow_runs | .[] | select(.head_sha|test("'$GITHUB_SHA'")|not) | select(.status|test("in_progress|queued")) | .id'))
+	runs=$(echo $runs $(curl -s -H "authorization: Bearer $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows/$wf/runs" | jq '.workflow_runs | .[] | select(.head_sha|test("'$COMMIT_SHA'")|not) | select(.status|test("in_progress|queued")) | .id'))
 done
 
 for run in $runs; do
