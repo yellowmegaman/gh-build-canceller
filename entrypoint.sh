ba1b811a7e0a23dc3cd9dc673f82fcfa6f11957a
yellowmegaman <yellowmegaman@gmail.com>
2020-03-04 16:30:19 +0300

    no time to explain

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,21 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
   exit 1
 fi
 
-CURL_HEADERS='-H "authorization: Bearer '$GITHUB_TOKEN'" -H "Accept: application/vnd.github.v3+json"'
 GITHUB_URL="https://api.github.com/repos"
-COMMIT_SHA=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_sha')
-COMMIT_TIMESTAMP=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_commit.timestamp')
+COMMIT_SHA=$(curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_sha')
+COMMIT_TIMESTAMP=$(curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID" | jq -r '.head_commit.timestamp')
 echo "COMMIT_SHA: $COMMIT_SHA"
 
-workflows=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows"  | jq -r '.workflows | .[] | select(.name|test("'$INPUT_WORKFLOWS_FILTER'")) | .id')
+workflows=$(curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows"  | jq -r '.workflows | .[] | select(.name|test("'$INPUT_WORKFLOWS_FILTER'")) | .id')
 
 for wf in $workflows; do
-	runs=$(echo $runs $(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows/$wf/runs" | jq -r '.workflow_runs | .[] | select((.head_sha|test("'$COMMIT_SHA'")|not) and (.status|test("in_progress|queued"))) | .id'))
+	runs=$(echo $runs $(curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/workflows/$wf/runs" | jq -r '.workflow_runs | .[] | select((.head_sha|test("'$COMMIT_SHA'")|not) and (.status|test("in_progress|queued"))) | .id'))
 done
 
 for run in $runs; do
-	run_timestamp=$(curl -s "$CURL_HEADERS" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run" | jq -r '.head_commit.timestamp')
+	run_timestamp=$(curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X GET "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run" | jq -r '.head_commit.timestamp')
 	if [[ "$COMMIT_TIMESTAMP" > "$run_timestamp" ]]; then
 		echo "We're about to cancel target: $run $run_timestamp"
-                curl -s "$CURL_HEADERS" -X POST "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run/cancel"
+                curl -s -H authorization: Bearer "$GITHUB_TOKEN" -X POST "$GITHUB_URL/$GITHUB_REPOSITORY/actions/runs/$run/cancel"
 	fi
 done
